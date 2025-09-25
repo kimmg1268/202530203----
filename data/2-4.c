a425b98182602138e904f4ad360a5a0825e75612
developern1 <developern.kimmg1268@gmail.com>
2025-09-25 12:17:04 +0900

    p53

---
+++ data/2-4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int na;
+    printf("요소 개수: ");
+    scanf("%d", &na);
+    int*a = calloc(na, sizeof(int));
+
+    if(a == NULL)
+        puts("메모리 확보에 실패했습니다.");
+    else{
+        printf("%d개의 정수를 입력하세요.\n", na);
+        for(int i = 0; i < na; i++){
+            printf("a[%d]: ", i);
+            scanf("%d", &a[i]);
+        }
+        printf("각 요솟값은 아래와 같습니다.\n");
+        for(int i = 0; i < na; i++)
+            printf("a[%d] = %d\n", i, a[i]);
+        free(a);
+    }
+
+    return 0;
+}
